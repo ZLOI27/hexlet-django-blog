367f0825bec03a0ec52b24629af113713c78348f
Kirill Zlobinov <zlobinovk@yandex.ru>
2026-04-03 22:42:06 +0300

    add: index to article

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("article")
+    app_name = request.resolver_match.app_name
+    context = {
+        'app_name': app_name,
+    }
+    return render(
+        request,
+        'articles/index.html',
+        context,
+    )
 
 # Create your views here.
